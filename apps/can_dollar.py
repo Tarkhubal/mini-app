1bab20773c9ec72d3e337d36f4e3aec648f022cf
The_WildThomas <thb5309@gmail.com>
2023-10-09 20:47:09 +0200

    Mini update with f-strings

--- apps/can_dollar.py
+++ apps/can_dollar.py
@@ -1,20 +1,18 @@
 def canadian_dollar():
     # Formule de bienvenue (pour la présentation)
-    print("\n\n" + '{:━^61}'.format(' Et si on convertissait des monnaies ? '))
+    print('{:━^61}'.format(' Et si on convertissait des monnaies ? '))
 
     # On initialise les variables
     dollar = 1.65   # Prix d'un dollar canadien en euros
     euros = 1       # Euro actuel
-    n = 1           # Numéro d'affichage dans la liste
 
     repeat = int(input("Combien de lignes souhaitez-vous générer (nombre entier strictement supérieur) ? "))
 
     # On créé une boucle qui va répéter 8 fois notre programme
-    for loop in range(8):
+    for i in range(repeat):
         # On affiche une phrase qui nous permet d'afficher le résultat de la conversion
-        print(str(n) + ". " + str(euros) + " euros = " + str(dollar * euros) + " $CAN")
+        print(f"{i+1}. {euros} euros = {dollar * euros} $CAN")
         # On ajoute 1 au numéro d'affichage dans la liste puis on multiplie l'euro actuel par 2
-        n += 1
         euros *= 2
 
     # Formule de au revoir (pour la présentation)
